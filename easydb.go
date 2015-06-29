20cfb60cfd48b9694db59b566c760d7142f2a7fe
Remizov Mikhail <00keng@gmail.com>
2015-06-29 13:19:49 +0300

    gofmt по проекту

--- easydb.go
+++ easydb.go
@@ -1,23 +1,17 @@
 package easydb
 
-
-
 import (
-	"github.com/jmoiron/sqlx"
-	_ "github.com/go-sql-driver/mysql"
 	"database/sql"
 	"errors"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"strings"
-	)
-
-
+)
 
 var (
 	master *sqlx.DB //Общий пул коннектов к мастеру
-	slave *sqlx.DB //ОБщий пул коннектов к слейву
-	)
-
-
+	slave  *sqlx.DB //ОБщий пул коннектов к слейву
+)
 
 //-------------------------------------------------------------------------------------------------
 // Соединяемся с базой и открываем пул коннектов (master и slave)
@@ -25,33 +19,31 @@ var (
 func ConnectMaster(driverName string, connectString string) error {
 	var (
 		err error
-		)
+	)
 
 	master = nil
 	master, err = sqlx.Connect(driverName, connectString)
 
-/*
-- DB.SetMaxIdleConns(n int)
-- DB.SetMaxOpenConns(n int)
+	/*
+	   - DB.SetMaxIdleConns(n int)
+	   - DB.SetMaxOpenConns(n int)
 
-By default, the pool grows unbounded, and connections will be created whenever there isn't a free connection available in the pool. 
-You can use DB.SetMaxOpenConns to set the maximum size of the pool. 
-Connections that are not being used are marked idle and then closed if they aren't required. 
-To avoid making and closing lots of connections, 
-set the maximum idle size with DB.SetMaxIdleConns to a size that is sensible for your query loads.
+	   By default, the pool grows unbounded, and connections will be created whenever there isn't a free connection available in the pool.
+	   You can use DB.SetMaxOpenConns to set the maximum size of the pool.
+	   Connections that are not being used are marked idle and then closed if they aren't required.
+	   To avoid making and closing lots of connections,
+	   set the maximum idle size with DB.SetMaxIdleConns to a size that is sensible for your query loads.
 
-Отсюда: http://jmoiron.github.io/sqlx/
-*/
+	   Отсюда: http://jmoiron.github.io/sqlx/
+	*/
 
 	return err
 }
 
-
-
 func ConnectSlave(driverName string, connectString string) error {
 	var (
 		err error
-		)
+	)
 
 	slave = nil
 	slave, err = sqlx.Connect(driverName, connectString)
@@ -59,15 +51,12 @@ func ConnectSlave(driverName string, connectString string) error {
 	return err
 }
 
-
-
-
 //-------------------------------------------------------------------------------------------------
 // Выбор коннекта в зависимости от того, какая задача, и какие есть подключения:
 // - для select пробуем отдать slave-коннект, если он подключен
 // - во всех остальных случаях отдаем master-коннект
 //-------------------------------------------------------------------------------------------------
-func ChooseConnection(purpose string) (*sqlx.DB) {
+func ChooseConnection(purpose string) *sqlx.DB {
 	if purpose == "select" {
 		if slave != nil {
 			return slave
@@ -77,15 +66,13 @@ func ChooseConnection(purpose string) (*sqlx.DB) {
 	return master
 }
 
-
-
 //-------------------------------------------------------------------------------------------------
 // Получение одного элемента из запроса
 //-------------------------------------------------------------------------------------------------
 func Get(dest interface{}, query string, args ...interface{}) error {
 	var (
-		db *sqlx.DB 
-		)
+		db *sqlx.DB
+	)
 	db = ChooseConnection("select")
 
 	if db == nil {
@@ -95,15 +82,13 @@ func Get(dest interface{}, query string, args ...interface{}) error {
 	return db.Get(dest, query, args...)
 }
 
-
-
 //-------------------------------------------------------------------------------------------------
 // Получение массива из запроса
 //-------------------------------------------------------------------------------------------------
 func Select(dest interface{}, query string, args ...interface{}) error {
 	var (
-		db *sqlx.DB 
-		)
+		db *sqlx.DB
+	)
 	db = ChooseConnection("select")
 
 	if db == nil {
@@ -113,8 +98,6 @@ func Select(dest interface{}, query string, args ...interface{}) error {
 	return db.Select(dest, query, args...)
 }
 
-
-
 //-------------------------------------------------------------------------------------------------
 // Замена /*condition*/ на условие запроса
 //-------------------------------------------------------------------------------------------------
@@ -122,15 +105,13 @@ func Condition(query string, condition string) string {
 	return strings.Replace(query, "/*condition*/", condition, -1)
 }
 
-
-
 //-------------------------------------------------------------------------------------------------
 // Выполнение запросов на изменение
 //-------------------------------------------------------------------------------------------------
 func NamedExec(query string, arg interface{}) (sql.Result, error) {
 	var (
-		db *sqlx.DB 
-		)
+		db *sqlx.DB
+	)
 	db = ChooseConnection("update")
 
 	if db == nil {
